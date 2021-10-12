f179a654aed2f26b34c6779730b2bcec3e194eb3
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-10-11 22:56:29 -0400

    Adding Bootstrap styling to reading app

--- src/components/BlogReadingApp/TitleList.js
+++ src/components/BlogReadingApp/TitleList.js
@@ -1,30 +1,25 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function TitleList(props) {
+  // const [showBlogPost, setShowBlogPost] = useState(false);
 
-    // const [showBlogPost, setShowBlogPost] = useState(false);
+  const blogTitlesHTML = props.blogs.map((blog) => (
+    <aside style={{ width: "30%" }}>
+      <div class="btn-group-vertical" style={{ padding: "10px" }}>
+        <button
+          class="btn btn-info btn-large"
+          type="button"
+          onClick={() => props.selectBlog(blog.id)}
+        >
+          {blog.title}
+        </button>
+      </div>
+    </aside>
+  ));
 
-    const blogTitlesHTML = props.blogs.map(blog => (
-        <aside style={{ width:'30%'}}>
-            <li >
-                <button type="button" onClick={() => props.selectBlog(blog.id)}>{blog.title}</button>
-            </li>
-        </aside>
+  // console.log({blogsHTML})
 
-    ))
-
-    // console.log({blogsHTML})
-
-
-
-
-    return (
-        <ul>
-            {blogTitlesHTML}
-        </ul>
-    )
-
+  return <ul>{blogTitlesHTML}</ul>;
 }
 
 export default TitleList;
